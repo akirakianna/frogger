0083c0729041b65825c8377fb8559ff6d482d20d
akirakianna <kianna.akiralove@gmail.com>
2020-06-02 17:54:30 +0100

    commit 4

--- scripts/app.js
+++ scripts/app.js
@@ -12,15 +12,19 @@ function setUpGame() {
   let frogPosition = 76
   const width = 9
   const tiles = []
-  const lilyPadFinish = 4
+  const lilyPadFinish = [1, 4, 7]
   const carsRightDisplay = [54, 57, 60]
   const carsLeftDisplay = [47, 50, 53]
-  const roadDisplay = [45, 46, 48, 49, 51, 52, 55, 56, 58, 59, 61, 62]
+  const roadDisplay = [45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62] // change to all
   const logsLeftDisplay = [18, 19, 20, 23, 24, 25]
   const logsRightDisplay = [28, 29, 30, 33, 34, 35]
-  const riverDisplay = [21, 22, 26, 27, 31, 32]
+  const riverDisplay = [0, 1, 2, 3, 4, 5, 6, 7, 8, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35] // change to all
 
-
+  let lilyPadInterval
+  let logsLeftInterval
+  let logsRightInterval
+  let carsRightInterval
+  let carsLeftInterval
 
 
   //Creating tiles on grid
@@ -36,7 +40,9 @@ function setUpGame() {
 
   tiles[frogPosition].classList.add('frog')
 
-  tiles[lilyPadFinish].classList.add('lilypad')
+  lilyPadFinish.forEach(lilypad => {
+    tiles[lilypad].classList.add('lilypad')
+  })
 
   carsRightDisplay.forEach(car => {
     tiles[car].classList.add('carsRight')
@@ -58,47 +64,61 @@ function setUpGame() {
     tiles[log].classList.add('log')
   })
 
-  // logsLeftDisplay[0].forEach(log => {
-  //   tiles[log].classList.add('log')
-  // })
-
-  // logsLeftDisplay[1].forEach(log => {
-  //   tiles[log].classList.add('log')
-  // })
-
-  // logsRightDisplay[0].forEach(log => {
-  //   tiles[log].classList.add('log')
-  // })
-
-  // logsRightDisplay[1].forEach(log => {
-  //   tiles[log].classList.add('log')
-  // })
-
   riverDisplay.forEach(waterTile => {
     tiles[waterTile].classList.add('water')
   })
 
-  // Rendering game
+  // ------ RENDERING GAME - DOM ELEMENTS ------ //
 
   function renderGame() {
     tiles.forEach(tile => {
       tile.classList.remove('frog')
+      //strip everything off of your board
+      tile.classList.remove('log')
+      tile.classList.remove('water')
+      tile.classList.remove('carsRight')
+      tile.classList.remove('carsLeft')
+      tile.classList.remove('road')
+      tile.classList.remove('lilypad')
     })
     tiles[frogPosition].classList.add('frog')
+    // add them back based on your logic, logs in array, 
+    // loops through logs display left and right for each
+    logsLeftDisplay.forEach((logPosition) => {
+      tiles[logPosition].classList.add('log')
+    })
+    logsRightDisplay.forEach((logPosition) => {
+      tiles[logPosition].classList.add('log')
+    })
+    riverDisplay.forEach((riverPosition) => {
+      tiles[riverPosition].classList.add('water')
+    })
+    carsLeftDisplay.forEach((carPosition) => {
+      tiles[carPosition].classList.add('carsLeft')
+    })
+    carsRightDisplay.forEach((carPosition) => {
+      tiles[carPosition].classList.add('carsRight')
+    })
+    roadDisplay.forEach((roadPosition) => {
+      tiles[roadPosition].classList.add('road')
+    })
+    lilyPadFinish.forEach((lilypadPosition) => {
+      tiles[lilypadPosition].classList.add('lilypad')
+    })
 
   }
 
 
-  // PLAYING THE GAME
+  // ------- PLAYING THE GAME ------- //
 
   startButton.addEventListener('click', () => {
     // swal({
     //   icon: "warning",
     //   text: `You have ${lives} lives left!`
     // });
+    startButton.classList.remove('bob-on-hover')
 
     // Creating movement for the frog
-    startButton.classList.remove('bob-on-hover')
 
     document.addEventListener('keydown', (e) => {
       if (e.key === 'ArrowRight') {
@@ -108,6 +128,7 @@ function setUpGame() {
         frogPosition += 1
         renderGame()
         gameOver()
+
       } else if (e.key === 'ArrowLeft') {
         if (frogPosition === 0) {
           return
@@ -115,6 +136,7 @@ function setUpGame() {
         frogPosition -= 1
         renderGame()
         gameOver()
+
       } else if (e.key === 'ArrowUp') {
         if (frogPosition < width) {
           return
@@ -122,6 +144,7 @@ function setUpGame() {
         frogPosition -= width
         renderGame()
         gameOver()
+
       } else if (e.key === 'ArrowDown') {
         if (frogPosition > (tiles.length - width - 1)) {
           return
@@ -129,229 +152,252 @@ function setUpGame() {
         frogPosition += width
         renderGame()
         gameOver()
+
       }
     })
 
-    // Creating game timer
+    // ------ GAME TIMER ------ //
 
     intervalId = setInterval(() => {
       count = count - 1
       if (count < 1) {
+        //not working ??
         clearInterval(intervalId)
       }
       timerDisplay.innerHTML = `You have ${count} seconds left!`
       if (count === 0) {
-        alert('Game Over!')
-        //reset game?
+        resetGame()
       }
     }, 1000)
 
-    // PLAYER WINS Function - where does this go?
+    // ------ CALLING FUNCTIONS ------ //
+    // Small delay ???
 
-    function playerWins() {
-      // if frog reaches lilypad before time runs out
-      if (tiles[lilyPadFinish].classList.contains('frog')) {
-        console.log('You won!')
-        document.removeEventListener('keydown')
-      }
-    }
-
-    //playerWins()
-
-
-    // GAME OVER Function  - doesn't quite work?
-    // also still going to - figures - how do I fix this?? Kind of a mess
-
-    function gameOver() {
-      if (tiles[frogPosition].classList.contains('carsRight') || tiles[frogPosition].classList.contains('carsLeft') ||
-        tiles[frogPosition].classList.contains('water')) {
-        lives = lives -= 1
-        livesDisplay.innerHTML = `Lives Remaining: ${lives}`
-        alert(`You have ${lives} lives left!`)
-        if (lives === 0) {
-          //resetFrog()
-          alert('Game Over!')
-        }
-      }
-    }
-
+    moveCarsRight()
+    moveCarsLeft()
+    moveLogsRight()
+    moveLogsLeft()
+    moveLilyPads()
 
   })
 
-  // RESET FROG FUNCTION - need help with this/ what it should do
+  // ------ GAME FUNCTIONS ------ //
 
-  function resetFrog() {
-    tiles[frogPosition].classList.remove('frog')
-    tiles[76].classList.add('frog')
+  // ------ Player Wins ------ //
+
+  function playerWins() {
+    if (tiles[frogPosition].classList.contains('lilyPad')) {
+      console.log('you won')
+    }
+  }
+  // does this need to be called somewhere specific?
+
+  // ------- Game Over ------ //
+
+
+  function gameOver() {
+    if (tiles[frogPosition].classList.contains('road') && tiles[frogPosition].classList.contains('carsLeft')
+      || tiles[frogPosition].classList.contains('carsRight')) {
+      resetFrog()
+      // lives = lives -= 1
+      // livesDisplay.innerHTML = `Lives Remaining: ${lives}`
+      // alert(`You have ${lives} lives left!`)
+      // if (lives === 0) {
+      //   resetGame()
+      // }
+    }
+    if (tiles[frogPosition].classList.contains('water') && !tiles[frogPosition].classList.contains('lilypad') && !tiles[frogPosition].classList.contains('log')) {
+      resetFrog()
+      // lives = lives -= 1
+      // livesDisplay.innerHTML = `Lives Remaining: ${lives}`
+      // alert(`You have ${lives} lives left!`)
+      // if (lives === 0) {
+      //   resetGame()
+      // }
+    }
   }
 
-  // CREATING CAR MOVEMENT
+  // ------ Reset Game (kind of) ------ //
+  // veeeeery glitchy doesn't reset everything
+  //called when lives === 0, or time has ran out 
+  function resetGame() {
+    alert('Game Over!')
+    
+    clearInterval(intervalId)
+    
+    // clear all movement intervals???
+    // can you put all in one set interval??
+    clearInterval(lilyPadInterval)
+    clearInterval(carsRightInterval)
+    clearInterval(carsLeftInterval)
+    clearInterval(logsLeftInterval)
+    clearInterval(logsRightInterval)
+    
+    document.removeEventListener('keydown', event)
+    document.removeEventListener('click', event)
 
-  // Moving cars right
+    //frog isn't returning to starting position
 
-  function moveCarsRight() {
-    setInterval(() => {
-      carsRightDisplay.forEach((car, i) => {
-        tiles[carsRightDisplay[i]].classList.remove('carsRight')
-        tiles[carsRightDisplay[i]].classList.add('road')
-        if (car === 62) {
-          carsRightDisplay[i] = 54
+
+    // count = 30
+    // lives = 3
+    // timerDisplay.innerHTML = `You have ${count} seconds left!`
+    // livesDisplay.innerHTML = `Lives Remaining: ${lives}`
+  
+  }
+
+
+  // ------ Reset Frog ------ //
+  // When you lose a life - if lives == 0 call resetGame function [see above]
+
+  function resetFrog() {
+    //removes frog from current position and places it back at starting position
+    frogPosition = 76
+    lives = lives -= 1
+    livesDisplay.innerHTML = `Lives Remaining: ${lives}`
+    alert(`You have ${lives} lives left!`)
+    if (lives === 0) {
+      resetGame()
+    }
+    
+  }
+ 
+  // ------ MOVING PIECES SECTION ------ //
+
+  // ------ Lilypad Movement ------ //
+
+  function moveLilyPads() {
+    lilyPadInterval = setInterval(() => {
+      lilyPadFinish.forEach((lilypad, i) => {
+        if (lilypad === 8) {
+          lilyPadFinish[i] = 0
 
         } else {
-          carsRightDisplay[i] += 1
-
+          lilyPadFinish[i] += 1
         }
-        tiles[carsRightDisplay[i]].classList.add('carsRight')
-        tiles[carsRightDisplay[i]].classList.remove('road')
-
       })
+      renderGame()
+    }, 500)
+
+  }
+
+  // ------ CAR MOVEMENT ------ //
+
+  // ------ Moving cars right ------ //
+
+  function moveCarsRight() {
+    carsRightInterval = setInterval(() => {
+      carsRightDisplay.forEach((car, i) => {
+        // tiles[carsRightDisplay[i]].classList.remove('carsRight')
+        // tiles[carsRightDisplay[i]].classList.add('road')
+        if (car === 62) {
+          carsRightDisplay[i] = 54
+        } else {
+          carsRightDisplay[i] += 1
+        }
+      })
+      renderGame()
     }, 1000)
   }
 
-  moveCarsRight()
-
-  // Moving cars left
+  // ------ Moving cars left ------ //
 
   function moveCarsLeft() {
-    setInterval(() => {
+    carsLeftInterval = setInterval(() => {
       carsLeftDisplay.forEach((car, i) => {
-        tiles[carsLeftDisplay[i]].classList.remove('carsLeft')
-        tiles[carsLeftDisplay[i]].classList.add('road')
+        // tiles[carsLeftDisplay[i]].classList.remove('carsLeft')
+        // tiles[carsLeftDisplay[i]].classList.add('road')
         if (car === 45) {
           carsLeftDisplay[i] = 53
         } else {
           carsLeftDisplay[i] -= 1
         }
-        tiles[carsLeftDisplay[i]].classList.add('carsLeft')
-        tiles[carsLeftDisplay[i]].classList.remove('road')
+        // tiles[carsLeftDisplay[i]].classList.add('carsLeft')
+        // tiles[carsLeftDisplay[i]].classList.remove('road')
       })
+      renderGame()
     }, 1000)
   }
 
-  moveCarsLeft()
+  // ------ LOG MOVEMENT ------ //
 
-  // CREATING LOG MOVEMENT
+  // ------ Moving logs right, inc. frog with log ------ //
 
-  //Moving logs right
+  // ------ THERE IS A GLITCH HERE AROUND TILES 32 ------ //
+
+  // moveLogsRight()
 
   function moveLogsRight() {
-    setInterval(() => {
-      for (let i = 0; i < logsRightDisplay.length; i++) {
-        tiles[logsRightDisplay[i]].classList.remove('log')
-        tiles[logsRightDisplay[i]].classList.add('water')
+    logsRightInterval = setInterval(() => {
+      logsRightDisplay.forEach((log, i) => {
+        if (log === frogPosition) {
+          if (log === 35) {
+            logsRightDisplay[i] = 27
+            
+            resetFrog()
+            
+          } else if (log !== 35) {
+            logsRightDisplay[i] += 1
+            frogPosition += 1
+          } else {
+            logsRightDisplay[i] += 1
+          }
 
-      }
-      for (let i = 0; i < logsRightDisplay.length; i++) {
-        if (logsRightDisplay[i] === 35) {
-          logsRightDisplay[i] = 27
         } else {
-          logsRightDisplay[i] += 1
+          if (log === 35) {
+            logsRightDisplay[i] = 27
+          } else {
+            logsRightDisplay[i] += 1
+          }
         }
-
-      }
-      for (let i = 0; i < logsRightDisplay.length; i++) {
-        tiles[logsRightDisplay[i]].classList.add('log')
-        tiles[logsRightDisplay[i]].classList.remove('water')
-
-      }
-
+      })
+      renderGame()
     }, 1000)
   }
 
-  moveLogsRight()
-
-  // function moveLogsRight() {
-  //   setInterval(() => {
-  //     logsRightDisplay.forEach((log, i) => {
-  //       tiles[logsRightDisplay[i]].classList.remove('log')
-  //       tiles[logsRightDisplay[i]].classList.add('water')
-  //       if (log === 35) {
-  //         logsRightDisplay[i] = 27
-  //       } else {
-  //         logsRightDisplay[i] += 1
-  //       }
-  //       tiles[logsRightDisplay[i]].classList.add('log')
-  //       tiles[logsRightDisplay[i]].classList.remove('water')
-  //     })
-  //   }, 1000)
-  // }
-
-  //moveLogsRight()
-
-
-
-  // Moving logs left
-  
+  // ------ Moving logs left inc. frog with log movement ------ //
 
   function moveLogsLeft() {
-    setInterval(() => {
+    logsLeftInterval = setInterval(() => {
       logsLeftDisplay.forEach((log, i) => {
+        //if the log has the frog on it
         if (log === frogPosition) {
+          //if the log has the frog on it and the index of the tile is 18 (far left)
           if (log === 18) {
-            //alert('DEAD')
-          } else {
-            // if frog is on log, need to remove both classes and add water
-            tiles[frogPosition].classList.remove('frog')
-            tiles[logsLeftDisplay[i]].classList.remove('log')
-            tiles[logsLeftDisplay[i]].classList.add('water')
+            logsLeftDisplay[i] = 26
+            resetFrog()
+            //game over lose lives function
+            //has to go below or logs won't reset - you want logs to move on then frog dies
+            // if the log index isn't 18 continue to move frog with log
+          } else if (log !== 18) {
             // move them back one step
             logsLeftDisplay[i] -= 1
             frogPosition -= 1
-            // once moved add them back, so they appear....
-            tiles[frogPosition].classList.add('frog')
-            tiles[logsLeftDisplay[i]].classList.add('log')
-            tiles[logsLeftDisplay[i]].classList.remove('water')
-          }
-        } else {
-          tiles[logsLeftDisplay[i]].classList.remove('log')
-          tiles[logsLeftDisplay[i]].classList.add('water')
-          if (log === 18) {
-            logsLeftDisplay[i] = 26
+            // once moved add them back, so they appear again on next tile (continuous movement)
+            //if frog is no longer there continue to loop logs
           } else {
             logsLeftDisplay[i] -= 1
           }
-          tiles[logsLeftDisplay[i]].classList.add('log')
-          tiles[logsLeftDisplay[i]].classList.remove('water')
+          // if frog was never there loop logs left
+        } else {
+          if (log === 18) {
+            logsLeftDisplay[i] = 26
+
+          } else {
+            logsLeftDisplay[i] -= 1
+
+          }
         }
       })
+      renderGame()
     }, 1000)
   }
 
-  moveLogsLeft()
-
-  // MOVING FROG WITH LOGS
-
-  //Left 
-
-  // WHYYYYYYY
-
-  function frogWithLogLeft() {
-    if (tiles[frogPosition].classList.contains('log') && tiles[frogPosition].classList.contains('frog')) {
-      tiles[frogPosition].classList.remove('frog')
-      frogPosition -= 1
-      tiles[frogPosition].classList.add('frog')
-    } else if (tiles[frogPosition] === 18) {
-      alert('Game Over')
-    }
-  }
-  frogWithLogLeft()
 
 
 
-  // function frogWithLogLeft() {
-  //   if (tiles[frogPosition] >= 18 && tiles[frogPosition] < 26) {
-  //     tiles[frogPosition].classList.remove('frog')
-  //     frogPosition -= 1
-  //     tiles[frogPosition].classList.add('frog')
-
-  //   }
-  // }
-
-  //Right
-
-
-
-  // RULES MODAL
+  // ------ RULES MODAL ------ //
+  // edit later 
 
   const modal = document.querySelector('.modal')
   const rules = document.querySelector('.rules')
